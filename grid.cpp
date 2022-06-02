dc05b8d001c431a85df20c9f256fa7af8a2c3aed
Anas_Imloul <76872415+AnasImloul@users.noreply.github.com>
2022-06-02 17:09:09 +0100

    Update grid.cpp
    std::cout is now supporting 2048 Grid objects 'Grid'.

--- grid.cpp
+++ grid.cpp
@@ -1,12 +1,18 @@
+#include "move.cpp"
+#include <cstring>
+
+typedef Move (*Moves) (long* grid, const int &rows, const int &columns);
 
 
 
-
-
-
-
-
-
+int digit(long number){
+	int digits = 1;
+	while (number >= 10){
+		digits++;
+		number = number / 10;
+	}
+	return digits;
+}
 
 
 class Grid{
@@ -17,7 +23,51 @@ class Grid{
 	
 	long score;
 	
+	Moves moves[4] = {up,down,left,right};
+	
+	public:
+	Grid(int rows, int columns){
+		this->rows = rows;
+		this->columns = columns;
+		
+		this->grid = (long*) malloc(sizeof(long)*4*4);	
+		
+		score = 0;
+	}
 	
 	
+	void Up(){
+		Move move = up(grid, rows, columns);
+		
+		this->grid = move.grid;
+		score += move.score;
+	}
+	
+	void Down(){
+		Move move = down(grid, rows, columns);
+		
+		this->grid = move.grid;
+		score += move.score;
+	}
+	
+	void Left(){
+		Move move = left(grid, rows, columns);
+		
+		this->grid = move.grid;
+		score += move.score;
+	}
+	
+	private:
+	
+	friend std::ostream& operator<<(std::ostream& os, const Grid& g){
+		std::string result = "";
+		for (int i = 0; i<g.rows*g.columns; i++){
+			result = result + std::to_string(g.grid[i]) + (((i==0 && g.columns != 1) || (i+1)%g.columns) ? " " : "\n");
+		}
+		result += "Score: " + std::to_string(g.score);
+		
+		return os << result;
+        
+	}
 	
 };
