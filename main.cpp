ab0d272db69cde0a43d81fb485f1343b11e7ab8e
Anas_Imloul <76872415+AnasImloul@users.noreply.github.com>
2022-06-03 00:07:14 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,51 +1,36 @@
 #include<iostream>
-#include "move.cpp"
-#include <vector>
+#include "grid.cpp"
 #include<chrono>
+#include "move.cpp"
+#include <algorithm>
 
 int64_t get_time(){
 	return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
 }
 
 
-int digit(long number){
-	int digits = 1;
-	while (number >= 10){
-		digits++;
-		number = number / 10;
-	}
-	return digits;
-}
-
-
-
-void show(long* grid, int rows, int columns){
-        for (int i = 0; i<rows*columns; i++){
-        	std::cout << grid[i] << (((i==0 && columns != 1) || (i+1)%columns) ? " " : "\n");
-       }
-}
-
-int main(){
-
-	std::vector<Move> arr;
-	
-	long grid[16] = {2,0,2,0,2,0,2,0,2,0,2,0,2,4,8,8};
-	
-	show(grid, 4,4);
+int main(){	
+	Grid grid = Grid(4,4);
 	
 	int64_t start = get_time();
 	
-	for (int i = 0; i<10'000'000; i++){
-	Move move = left(grid,4,4);
+	for (int i = 0; i<20;i++){
+		grid.add();
 	}
 	
-	std::cout << get_time() - start << std::endl;
 	
-	//arr.push_back(Move(0,1013,false,grid,0));
+	grid.up();
 	
-	//show(move.grid, 4,4);
+	grid.left();
+	
+	//grid.play(1,4,true);
+	
+	
+	MoveTracker best_move = maxsearch(grid.grid, grid.rows, grid.columns, grid.empty_slots, grid.moves, 4,10);
 	
 	
 		
-
+	std::cout << best_move.path.size() << std::endl;
+	
+	std::cout << get_time() - start << std::endl;
 }
